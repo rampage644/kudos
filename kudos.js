eaa70f2224303ae174c22b3206964d5eb050a9ed
Sergei Turukin <rampage644@gmail.com>
2015-10-31 20:30:28 -0500

    Simple search bar with auto suggest feature

--- kudos.js
+++ kudos.js
@@ -1,6 +1,27 @@
+users = new Mongo.Collection('users');
+
+
 if (Meteor.isClient) {
-    // client code goes here
+    Meteor.startup(function() {
+      Meteor.typeahead.inject();
+    });
+
+    Template.searchbar.events({
+        "submit": function(event) {
+            event.preventDefault();
+            console.log(event);
+        }
+    });
+
+    Template.searchbar.helpers({
+        users: function() {
+            return users.find().map(function(it) {
+                return it.fullname;
+            });
+        }
+    });
 }
+
 
 if (Meteor.isServer) {
     // server code goes here
