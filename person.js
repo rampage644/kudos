f468ae668a39c51d53927d803cc6ca1378498b1c
Sergei Turukin <rampage644@gmail.com>
2015-11-07 15:16:52 -0600

    Done loading users from LDAP directory to mongo collection

--- person.js
+++ person.js
@@ -13,46 +13,53 @@ if (Meteor.isClient) {
     })
 }
 
+function syncUsers() {
+    if (users.find().count() !== 0)
+        return;
+
+    var ldap = Meteor.npmRequire('ldapjs');
+    var client = ldap.createClient({
+        url:'ldaps://auth.edir.rackspace.com',
+    });
+
+    var user = 'cn=kudos_app,ou=Users,o=rackspace';
+    var passwd = '';
+
+    var bindSync = Async.wrap(client, 'bind');
+    bindSync(user, passwd);
+
+
+    var base = 'o=rackspace'
+    var opts = {
+        filter:'(&(photo=*)(cn=*))', 
+        scope:'sub', 
+        attributes: ['dn', 'cn', 'title', 'sn', 'givenName', 'Division', 'manager', 'displayName', 'photo'],
+        timeLimit: 60
+    };
+
+
+    var searchSync = Async.wrap(client, 'search');
+    var results = searchSync(base, opts);
+
+    results.on('searchEntry', Meteor.bindEnvironment(function(entry) {
+        users.insert({
+            'firstName': entry.object.givenName,
+            'lastName': entry.object.sn,
+            'fullName': entry.object.displayName,
+            'title': entry.object.title,
+            'photo': entry.object.photo,
+            'department': entry.object.Division
+        });
+    }));
+    results.on('error', function(err) {
+    });
+
+    results.on('end', function(){
+    })
+};
+
 if (Meteor.isServer) {
-    if (users.find().count() === 0) {
-        var ldap = Meteor.npmRequire('ldapjs');
-        var client = ldap.createClient({
-            url:'ldaps://auth.edir.rackspace.com'
-        });
-
-        var user = 'cn=kudos_app,ou=Users,o=rackspace';
-        var passwd = '';
-
-        Async.runSync(function(done) {
-            client.bind(user, passwd, function(err) {
-                done(null, 0);
-            });
-        });
-
-        var base = 'o=rackspace'
-        var opts = {
-            filter:'(&(photo=*)(cn=*)(cn=serg7400))', 
-            scope:'sub', 
-            attributes: ['dn', 'cn', 'title', 'sn', 'givenName', 'Division', 'manager', 'displayName', 'photo']
-        };
-
-        Async.runSync(function(done) {
-            client.search(base, opts, function(err, res) { 
-                res.on('searchEntry', Meteor.bindEnvironment(function(entry) {
-                    users.insert({
-                        'firstName': entry.object.givenName,
-                        'lastName': entry.object.sn,
-                        'fullName': entry.object.displayName,
-                        'title': entry.object.title,
-                        'photo': entry.object.photo,
-                        'department': entry.object.Division
-                    });
-                }));
-                res.on('end', function(res) {
-                    console.log('done loading search results');
-                    done(null, 0);
-                });
-            });
-        });
-    }
+    Meteor.methods({
+        'syncUsers': syncUsers
+    });
 }
